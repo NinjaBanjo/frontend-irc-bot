83b1c3d9a62694838b9935866bfe5fc7eed60afa
Derek Persons <derek.persons@gmail.com>
2014-09-07 12:05:11 -0500

    adding factoid system

---
+++ plugins/factoid/index.js
@@ -0,0 +1,82 @@
+var Bot = require('../../lib/bot');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('factoid.sqlite');
+
+var factoid = function () {
+};
+
+factoid.prototype.init = function (config, bot) {
+  factoid.__bot = bot;
+  factoid.__config = config;
+};
+
+factoid.prototype.registerCommands = function () {
+  var self = this;
+  Bot.prototype.registerCommand.call(this, 'set', 'factoid', 'set');
+  Bot.prototype.registerCommand.call(this, 'delete', 'factoid', 'delete');
+
+  db.serialize(function () {
+    db.run('CREATE TABLE IF NOT EXISTS factoids (id INTEGER PRIMARY KEY AUTOINCREMENT, command TEXT, value TEXT)');
+
+    db.each("SELECT id, command FROM factoids", function (err, row) {
+      Bot.prototype.registerCommand.call(self, row.command, 'factoid', 'factoid');
+    });
+  });
+};
+
+factoid.set = function (client, command, params, from, to) {
+  if (from == 'NinjaBanjo' || 'drej') {
+    var paramsSplit = params.trim().split(/ (.+)/);
+    var newCommand = paramsSplit[0];
+    var value = paramsSplit[1];
+    db.serialize();
+    db.get("SELECT id FROM factoids WHERE command = ?", {1: newCommand}, function (err, row) {
+      if (row === undefined) {
+        db.run("INSERT INTO factoids (command, value) VALUES (?1, ?2)", {1: newCommand, 2: value});
+        Bot.prototype.registerCommand.call(factoid.__bot, newCommand, 'factoid', 'factoid');
+        client.notice(from, "Command Inserted Successfully!");
+      } else {
+        db.run("UPDATE factoids SET value = ?1 WHERE id = ?2", {1: value, 2: row.id});
+        client.notice(from, "Command Updated Successfully!");
+      }
+    });
+
+  } else {
+    client.say(to, from + ': You are not authorized to use this command');
+    Bot.prototype.log('Unauthorized attempt to use !set in factoid by ' + to);
+  }
+};
+
+factoid.delete = function(client, command, params, from, to) {
+  if(from == 'NinjaBanjo' || 'drej') {
+    db.serialize(function() {
+      db.get("SELECT id FROM factoids WHERE command = ? LIMIT 1", {1: params}, function(err, row){
+        if(row !== undefined) {
+          db.run("DELETE FROM factoids WHERE id = ?", {1: row.id});
+          Bot.prototype.unregisterCommand.call(factoid.__bot, params);
+          client.notice(from, "Command deleted successfully");
+        } else {
+          client.notice(from, "Command does not exist, could not delete");
+        }
+      });
+    });
+  } else {
+    client.notice(from, "You are not authorized to use that command");
+    Bot.prototype.log('Unauthorized attempt to use !delete in factoid by ' + to);
+  }
+};
+
+factoid.factoid = function (client, command, params, from, to) {
+  // Get command from db and return value to user
+  db.serialize(function () {
+    db.get("SELECT command,value FROM factoids WHERE command = ? LIMIT 1", {1: command}, function (err, row) {
+      if (row !== undefined) {
+        client.say(to, from + ': ' + row.value);
+      } else {
+        Bot.prototype.log('Error: factoid not found ' + command);
+      }
+    });
+  });
+};
+
+module.exports = factoid;

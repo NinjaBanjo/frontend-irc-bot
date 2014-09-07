a1d9d0c365d5093be3b996e7392ce1667e96f72d
Derek Persons <derek.persons@gmail.com>
2014-09-07 12:39:54 -0500

    adding factoids list request and fixing authentication for delete in factoids

--- plugins/factoid/index.js
+++ plugins/factoid/index.js
@@ -12,6 +12,7 @@ factoid.prototype.init = function (config, bot) {
 
 factoid.prototype.registerCommands = function () {
   var self = this;
+  Bot.prototype.registerCommand.call(this, 'factoids', 'factoid', 'list');
   Bot.prototype.registerCommand.call(this, 'set', 'factoid', 'set');
   Bot.prototype.registerCommand.call(this, 'delete', 'factoid', 'delete');
 
@@ -26,7 +27,6 @@ factoid.prototype.registerCommands = function () {
 
 factoid.set = function (client, command, params, from, to) {
   client.whois(from, function (res) {
-    console.log(res);
     if (res !== undefined && res.account !== undefined && res.account === 'NinjaBanjo' || res.account === 'jedimind') {
       var paramsSplit = params.trim().split(/ (.+)/);
       var newCommand = paramsSplit[0];
@@ -50,22 +50,47 @@ factoid.set = function (client, command, params, from, to) {
 };
 
 factoid.delete = function (client, command, params, from, to) {
-  if (from == 'NinjaBanjo' || 'drej') {
-    db.serialize(function () {
-      db.get("SELECT id FROM factoids WHERE command = ? LIMIT 1", {1: params}, function (err, row) {
-        if (row !== undefined) {
-          db.run("DELETE FROM factoids WHERE id = ?", {1: row.id});
-          Bot.prototype.unregisterCommand.call(factoid.__bot, params);
-          client.notice(from, "Command deleted successfully");
-        } else {
-          client.notice(from, "Command does not exist, could not delete");
-        }
+  client.whois(from, function (res) {
+    if (res !== undefined && res.account !== undefined && res.account === 'NinjaBanjo' || res.account === 'jedimind') {
+      db.serialize(function () {
+        db.get("SELECT id FROM factoids WHERE command = ? LIMIT 1", {1: params}, function (err, row) {
+          if (row !== undefined) {
+            db.run("DELETE FROM factoids WHERE id = ?", {1: row.id});
+            Bot.prototype.unregisterCommand.call(factoid.__bot, params);
+            client.notice(from, "Command deleted successfully");
+          } else {
+            client.notice(from, "Command does not exist, could not delete");
+          }
+        });
       });
+    } else {
+      client.notice(from, "You are not authorized to use that command");
+      Bot.prototype.log('Unauthorized attempt to use !delete in factoid by ' + to);
+    }
+  });
+};
+
+factoid.list = function (client, command, params, from, to) {
+  var factoids = '';
+  var i = 1;
+  db.serialize(function () {
+    db.each("SELECT command FROM factoids ORDER BY command ASC", function (err, row) {
+      if (row !== undefined) {
+        if (i === 1) {
+          factoids = row.command;
+        } else {
+          factoids = factoids + ', ' + row.command;
+        }
+        i++;
+      }
+    }, function (err, total) {
+      if (total === 0) {
+        client.notice(from, "There are currently no registered factoids");
+      } else {
+        client.notice(from, total + " Factoids: " + factoids);
+      }
     });
-  } else {
-    client.notice(from, "You are not authorized to use that command");
-    Bot.prototype.log('Unauthorized attempt to use !delete in factoid by ' + to);
-  }
+  });
 };
 
 factoid.factoid = function (client, command, params, from, to) {

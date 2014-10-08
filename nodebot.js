aa0eb21206edb5154a04a97fcf2ec47a91b745bc
Derek Persons <derek.persons@gmail.com>
2014-10-08 07:35:21 -0500

    updating the bot to prepare for adding auto update from master in the future

--- nodebot.js
+++ nodebot.js
@@ -6,9 +6,10 @@ var auth = require('./lib/auth');
 var config = require('./config.json');
 
 var NodeBot = function (config) {
+    // Set the working directory so we can use to allow to bot to update itself
+    this.workingDirectory = __dirname;
     // Load the bot
     Bot.call(this, config);
-
     // Load the plugin loader
     pluginLoader.call(this, config.plugins, Bot);
 };

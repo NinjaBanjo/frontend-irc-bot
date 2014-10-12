636be71084803fd3873e3896c6157342d43f4e91
Derek Persons <derek.persons@gmail.com>
2014-10-12 03:30:35 -0500

    updating an error in default configs for bot lib

--- plugins/adminCommands/index.js
+++ plugins/adminCommands/index.js
@@ -41,161 +41,98 @@ adminCommands.listRestrictions = function (client, command, params, from) {
         return 0;
     }
 
-    auth.authorize(client, command, from)
-        .then(function (res) {
-            if (typeof res === "object" && res.auth === true) {
-                var commands = auth.getAllCommands(),
-                    commandsList = '';
-                _.forEach(commands.sort(sortComparison), function (command, index) {
-                    var separator = (index < 1 ? '' : ', ');
-                    commandsList += separator + command.getCommand() + ':' + command.getValue();
-                });
+    var commands = auth.getAllCommands(),
+        commandsList = '';
+    _.forEach(commands.sort(sortComparison), function (command, index) {
+        var separator = (index < 1 ? '' : ', ');
+        commandsList += separator + command.getCommand() + ':' + command.getValue();
+    });
 
-                client.notice(from, commandsList);
-            } else {
-                client.notice(from, 'You\'re not authorized to use this command');
-            }
-        }, function (res) {
-            client.notice(from, res.message);
-        });
+    client.notice(from, commandsList);
 };
 
 adminCommands.restrict = function (client, command, params, from) {
-    auth.authorize(client, command, from)
-        .then(function (res) {
-            if (typeof res === "object" && res.auth === true) {
-                if (typeof params === "string" && params.length > 0) {
-                    var args = params.split(' ');
-                    auth.restrictCommand(args[0], args[1])
-                        .then(function (res) {
-                            client.notice(from, res.message);
-                        }, function (res) {
-                            client.notice(from, res.message);
-                        });
-                }
-            } else {
-                client.notice(from, 'You\'re not authorized to use this command');
-            }
-        }, function (res) {
-            client.notice(from, res.message);
-        });
+    if (typeof params === "string" && params.length > 0) {
+        var args = params.split(' ');
+        auth.restrictCommand(args[0], args[1])
+            .then(function (res) {
+                client.notice(from, res.message);
+            }, function (res) {
+                client.notice(from, res.message);
+            });
+    }
 };
 
 adminCommands.unrestrict = function (client, command, params, from) {
-    auth.authorize(client, command, from)
+    auth.unrestrictCommand(params)
         .then(function (res) {
-            if (typeof res === "object" && res.auth === true) {
-                auth.unrestrictCommand(params)
-                    .then(function (res) {
-                        client.notice(from, res.message);
-                    }, function (res) {
-                        client.notice(from, res.message);
-                    });
-            } else {
-                client.notice(from, 'You\'re not authorized to use this command');
-            }
+            client.notice(from, res.message);
         }, function (res) {
             client.notice(from, res.message);
         });
 };
 
 adminCommands.listUsers = function (client, command, params, from) {
-    auth.authorize(client, command, from)
-        .then(function (res) {
-            if (typeof res === "object" && res.auth === true) {
-                var users = auth.getAllUsers(),
-                    usersList = '';
-                if (users.length > 0) {
-                    _.forEach(users, function (user, index) {
-                        usersList += (index > 0 ? ', ' + user.getAccount() : user.getAccount());
-                    });
-                } else {
-                    usersList = 'There are currently no users in the system';
-                }
-                client.notice(from, usersList);
-            } else {
-                client.notice(from, 'You\'re not authorized to use this command');
-            }
-        }, function (res) {
-            client.notice(from, res.message);
+    var users = auth.getAllUsers(),
+        usersList = '';
+    if (users.length > 0) {
+        _.forEach(users, function (user, index) {
+            usersList += (index > 0 ? ', ' + user.getAccount() : user.getAccount());
         });
+    } else {
+        usersList = 'There are currently no users in the system';
+    }
+    client.notice(from, usersList);
 };
 
 adminCommands.addUser = function (client, command, params, from) {
-    auth.authorize(client, command, from)
-        .then(function (res) {
-            if (typeof res === "object" && res.auth === true) {
-                var args = params.split(' '),
-                    optionSets = _.rest(params, 1);
-                auth.getAccountName(client, args[0]).then(function (res) {
-                    auth.createUser(res.account, args[1])
-                        .then(function (res) {
-                            if (typeof res === "object") {
-                                client.notice(from, res.message);
-                            } else {
-                                throw new Error('res is not an object');
-                            }
-                        });
-                }, function (res) {
+    var args = params.split(' '),
+        optionSets = _.rest(params, 1);
+    auth.getAccountName(client, args[0]).then(function (res) {
+        auth.createUser(res.account, args[1])
+            .then(function (res) {
+                if (typeof res === "object") {
                     client.notice(from, res.message);
-                });
-            } else {
-                client.notice(from, 'You\'re not authorized to use this command');
-            }
-        }, function (res) {
-            client.notice(from, res.message);
-        });
+                } else {
+                    throw new Error('res is not an object');
+                }
+            });
+    }, function (res) {
+        client.notice(from, res.message);
+    });
 };
 
 adminCommands.updateUser = function (client, command, params, from, to) {
-    auth.authorize(client, command, from)
+    var args = params.split(' '),
+        optionsSet = _.rest(args, 1),
+        keys = _.groupBy(optionsSet, function (num, index) {
+            index++;
+            return index % 2;
+        });
+    keys = _.object(keys[1], keys[0]);
+    auth.updateUser(_.first(args), keys)
         .then(function (res) {
-            if (typeof res === "object" && res.auth === true) {
-                var args = params.split(' '),
-                    optionsSet = _.rest(args, 1),
-                    keys = _.groupBy(optionsSet, function (num, index) {
-                        index++;
-                        return index % 2;
-                    });
-                keys = _.object(keys[1], keys[0]);
-                auth.updateUser(_.first(args), keys)
-                    .then(function (res) {
-                        if (typeof res === "object") {
-                            client.notice(from, res.message);
-                        } else {
-                            throw new Error('res is not an object');
-                        }
-                    }, function (res) {
-                        if (typeof res === "object") {
-                            client.notice(from, res.message);
-                        } else {
-                            throw new Error('res is not an object');
-                        }
-                    });
+            if (typeof res === "object") {
+                client.notice(from, res.message);
             } else {
-                client.notice(from, 'You\'re not authorized to use this command');
+                throw new Error('res is not an object');
             }
         }, function (res) {
-            client.notice(from, res.message);
+            if (typeof res === "object") {
+                client.notice(from, res.message);
+            } else {
+                throw new Error('res is not an object');
+            }
         });
 };
 
 adminCommands.deleteUser = function (client, command, params, from, to) {
-    auth.authorize(client, command, from)
+    auth.deleteUser(params)
         .then(function (res) {
-            if (typeof res === "object" && res.auth === true) {
-                auth.deleteUser(params)
-                    .then(function (res) {
-                        if (typeof res === "object") {
-                            client.notice(from, res.message);
-                        } else {
-                            throw new Error('res is not an object');
-                        }
-                    }, function (res) {
-                        client.notice(from, res.message);
-                    });
+            if (typeof res === "object") {
+                client.notice(from, res.message);
             } else {
-                client.notice(from, 'You\'re not authorized to use this command');
+                throw new Error('res is not an object');
             }
         }, function (res) {
             client.notice(from, res.message);
@@ -203,142 +140,88 @@ adminCommands.deleteUser = function (client, command, params, from, to) {
 };
 
 adminCommands.listGroups = function (client, command, params, from) {
-    auth.authorize(client, command, from)
-        .then(function (res) {
-            if (typeof res === "object" && res.auth === true) {
-                var groups = auth.getAllGroups(),
-                    groupsList = '';
-                if (groups.length > 0) {
-                    _.forEach(groups, function (group, index) {
-                        groupsList += (index > 0 ? ', ' + group.getName() : group.getName());
-                    });
-                } else {
-                    groupsList = 'There are currently no users in the system';
-                }
-                client.notice(from, groupsList);
-            } else {
-                client.notice(from, 'You\'re not authorized to use this command');
-            }
-        }, function (res) {
-            client.notice(from, res.message);
+    var groups = auth.getAllGroups(),
+        groupsList = '';
+    if (groups.length > 0) {
+        _.forEach(groups, function (group, index) {
+            groupsList += (index > 0 ? ', ' + group.getName() : group.getName());
         });
+    } else {
+        groupsList = 'There are currently no users in the system';
+    }
+    client.notice(from, groupsList);
 };
 
 adminCommands.addGroup = function (client, command, params, from, to) {
-    auth.authorize(client, command, from)
+    var args = params.split(' ');
+    auth.createGroup(args[0], args[1])
         .then(function (res) {
-            if (typeof res === "object" && res.auth === true) {
-                var args = params.split(' ');
-                auth.createGroup(args[0], args[1])
-                    .then(function (res) {
-                        if (typeof res === "object") {
-                            client.notice(from, res.message);
-                        } else {
-                            throw new Error('res is not an object');
-                        }
-                    });
+            if (typeof res === "object") {
+                client.notice(from, res.message);
             } else {
-                client.notice(from, 'You\'re not authorized to use this command');
+                throw new Error('res is not an object');
             }
-        }, function (res) {
-            client.notice(from, res.message);
         });
 };
 
 adminCommands.updateGroup = function (client, command, params, from) {
-    auth.authorize(client, command, from)
+    var args = params.split(' '),
+        optionsSet = _.rest(args, 1),
+        keys = _.groupBy(optionsSet, function (num, index) {
+            index++;
+            return index % 2;
+        });
+    keys = _.object(keys[1], keys[0]);
+    auth.updateGroup(_.first(args), keys)
         .then(function (res) {
-            if (typeof res === "object" && res.auth === true) {
-                var args = params.split(' '),
-                    optionsSet = _.rest(args, 1),
-                    keys = _.groupBy(optionsSet, function (num, index) {
-                        index++;
-                        return index % 2;
-                    });
-                keys = _.object(keys[1], keys[0]);
-                auth.updateGroup(_.first(args), keys)
-                    .then(function (res) {
-                        if (typeof res === "object") {
-                            client.notice(from, res.message);
-                        } else {
-                            throw new Error('res is not an object');
-                        }
-                    }, function (res) {
-                        if (typeof res === "object") {
-                            client.notice(from, res.message);
-                        } else {
-                            throw new Error('res is not an object');
-                        }
-                    });
+            if (typeof res === "object") {
+                client.notice(from, res.message);
             } else {
-                client.notice(from, 'You\'re not authorized to use this command');
+                throw new Error('res is not an object');
             }
         }, function (res) {
-            client.notice(from, res.message);
+            if (typeof res === "object") {
+                client.notice(from, res.message);
+            } else {
+                throw new Error('res is not an object');
+            }
         });
 };
 
 adminCommands.deleteGroup = function (client, command, params, from) {
-    auth.authorize(client, command, from)
+    auth.deleteGroup(params)
         .then(function (res) {
-            if (typeof res === "object" && res.auth === true) {
-                auth.deleteGroup(params)
-                    .then(function (res) {
-                        if (typeof res === "object") {
-                            client.notice(from, res.message);
-                        } else {
-                            throw new Error('res is not an object');
-                        }
-                    });
+            if (typeof res === "object") {
+                client.notice(from, res.message);
             } else {
-                client.notice(from, 'You\'re not authorized to use this command');
+                throw new Error('res is not an object');
             }
-        }, function (res) {
-            client.notice(from, res.message);
         });
 };
 
 adminCommands.reloadPlugins = function (client, command, params, from, to) {
-    auth.authorize(client, command, from)
-        .then(function (res) {
-            if (typeof res === "object" && res.auth === true) {
-                client.whois(from, function (res) {
-                    if (res !== undefined && res.account !== undefined && res.account === 'NinjaBanjo' || res.account === 'jedimind') {
-                        adminCommands.__scope.plugins = [];
-                        adminCommands.__scope.commands = {};
-                        pluginLoader.prototype.init.call(adminCommands.__scope);
-                    } else {
-                        client.notice(from, 'You are not authorized to use that command');
-                        Bot.prototype.log('Unauthorized attempt to use `reload by ' + to);
-                    }
-                });
-            } else {
-                client.notice(from, 'You\'re not authorized to use this command');
-            }
-        }, function (res) {
-            client.notice(from, res.message);
-        });
+    client.whois(from, function (res) {
+        if (res !== undefined && res.account !== undefined && res.account === 'NinjaBanjo' || res.account === 'jedimind') {
+            adminCommands.__scope.plugins = [];
+            adminCommands.__scope.commands = {};
+            pluginLoader.prototype.init.call(adminCommands.__scope);
+        } else {
+            client.notice(from, 'You are not authorized to use that command');
+            Bot.prototype.log('Unauthorized attempt to use `reload by ' + to);
+        }
+    });
 };
 
 adminCommands.restart = function (client, command, params, from, to) {
-    auth.authorize(client, command, from)
-        .then(function (res) {
-            if (typeof res === "object" && res.auth === true) {
-                client.whois(from, function (res) {
-                    if (res !== undefined && res.account !== undefined && res.account === 'NinjaBanjo' || res.account === 'jedimind') {
-                        // Because the bot is meant to be run with forever by exiting the process forever will restart the bot for us
-                        process.exit(0);
-                    } else {
-                        client.notice(from, 'You are not authorized to use that command');
-                        Bot.prototype.log('Unauthorized attempt to use `restart by ' + to);
-                    }
-                });
-            } else {
-                client.notice(from, 'You\'re not authorized to use this command');
-            }
-        }, function (res) {
-            client.notice(from, res.message);
-        });
+    client.whois(from, function (res) {
+        if (res !== undefined && res.account !== undefined && res.account === 'NinjaBanjo' || res.account === 'jedimind') {
+            // Because the bot is meant to be run with forever by exiting the process forever will restart the bot for us
+            process.exit(0);
+        } else {
+            client.notice(from, 'You are not authorized to use that command');
+            Bot.prototype.log('Unauthorized attempt to use `restart by ' + to);
+        }
+    });
 }
 
 module.exports = adminCommands;
--- plugins/factoid/index.js
+++ plugins/factoid/index.js
@@ -45,80 +45,53 @@ factoid.set = function (client, command, params, from, to) {
 };
 
 factoid.delete = function (client, command, params, from, to) {
-    auth.authorize(client, command, from)
-        .then(function (res) {
-            if (typeof res === "object" && res.auth === true) {
-                db.serialize(function () {
-                    db.get("SELECT id FROM factoids WHERE command = ? LIMIT 1", {1: params}, function (err, row) {
-                        if (row !== undefined) {
-                            db.run("DELETE FROM factoids WHERE id = ?", {1: row.id});
-                            Bot.prototype.unregisterCommand.call(factoid.__scope, params);
-                            client.notice(from, "Command deleted successfully");
-                        } else {
-                            client.notice(from, "Command does not exist, could not delete");
-                        }
-                    });
-                });
+    db.serialize(function () {
+        db.get("SELECT id FROM factoids WHERE command = ? LIMIT 1", {1: params}, function (err, row) {
+            if (row !== undefined) {
+                db.run("DELETE FROM factoids WHERE id = ?", {1: row.id});
+                Bot.prototype.unregisterCommand.call(factoid.__scope, params);
+                client.notice(from, "Command deleted successfully");
             } else {
-                client.notice(from, 'You\'re not authorized to use this command');
+                client.notice(from, "Command does not exist, could not delete");
             }
-        }, function (res) {
-            client.notice(from, res.message);
         });
+    });
 };
 
 factoid.list = function (client, command, params, from, to) {
-    auth.authorize(client, command, from)
-        .then(function (res) {
-            if (typeof res === "object" && res.auth === true) {
-                var factoids = '',
-                    i = 1;
-                db.serialize(function () {
-                    db.each("SELECT command FROM factoids ORDER BY command ASC", function (err, row) {
-                        if (row !== undefined) {
-                            if (i === 1) {
-                                factoids = row.command;
-                            } else {
-                                factoids = factoids + ', ' + row.command;
-                            }
-                            i++;
-                        }
-                    }, function (err, total) {
-                        if (total === 0) {
-                            client.notice(from, "There are currently no registered factoids");
-                        } else {
-                            client.notice(from, total + " Factoids: " + factoids);
-                        }
-                    });
-                });
-            } else {
-                client.notice(from, 'You\'re not authorized to use this command');
+    var factoids = '',
+        i = 1;
+    db.serialize(function () {
+        db.each("SELECT command FROM factoids ORDER BY command ASC", function (err, row) {
+            if (row !== undefined) {
+                if (i === 1) {
+                    factoids = row.command;
+                } else {
+                    factoids = factoids + ', ' + row.command;
+                }
+                i++;
             }
-        }, function (res) {
-            client.notice(from, res.message);
+        }, function (err, total) {
+            if (total === 0) {
+                client.notice(from, "There are currently no registered factoids");
+            } else {
+                client.notice(from, total + " Factoids: " + factoids);
+            }
         });
+    });
 };
 
 factoid.factoid = function (client, command, params, from, to) {
-    auth.authorize(client, command, from)
-        .then(function (res) {
-            if (typeof res === "object" && res.auth === true) {
-                // Get command from db and return value to user
-                db.serialize(function () {
-                    db.get("SELECT command,value FROM factoids WHERE command = ? LIMIT 1", {1: command}, function (err, row) {
-                        if (row !== undefined) {
-                            client.say(to, from + ': ' + row.value);
-                        } else {
-                            Bot.prototype.log('Error: factoid not found ' + command);
-                        }
-                    });
-                });
+    // Get command from db and return value to user
+    db.serialize(function () {
+        db.get("SELECT command,value FROM factoids WHERE command = ? LIMIT 1", {1: command}, function (err, row) {
+            if (row !== undefined) {
+                client.say(to, from + ': ' + row.value);
             } else {
-                client.notice(from, 'You\'re not authorized to use this command');
+                Bot.prototype.log('Error: factoid not found ' + command);
             }
-        }, function (res) {
-            client.notice(from, res.message);
         });
+    });
 };
 
 module.exports = factoid;
